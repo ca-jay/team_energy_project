2956ab04d84f39ea52909a75e6b7edb899377cb4
Jay Dunn <jaydunn@csus.edu>
2022-06-28 00:04:41 -0700

    Added movie list

--- team_energy_project/src/Main.java
+++ team_energy_project/src/Main.java
@@ -1,3 +1,28 @@
 //TESTING THE FIRST PUSH
 
 //EDIT TO THE JAY_DEV BRANCH 
+
+
+
+// MOVIE LIST
+//
+//1. Top Gun - Maverick
+//2. Doctor Strange: Multiverse of Madness
+//3. Jurassic World: Dominion
+//4. The Batman
+//5. Lightyear
+//6. Uncharted
+//7. Death on the Nile
+//8.  Dog
+//9. The Lost City
+//10. Everything Everywhere all At Once
+//11. Ambulance
+//12. The Black Phone
+//13. The Bad Guys
+//14. Elvis
+//15. The Northman
+//16. The Unbearable Weight of Massive Talent
+//17. Sneakerella
+//18. Downton Abbey
+//19. The Bob's Burgers Movie
+//20. Father of the Bride
